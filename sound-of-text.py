5a59575bf36cb05798ff08535f67d97b950ac575
Max Engel <maxengel99@gmail.com>
2019-09-02 17:16:17 +0700

    can download multiple numbers"

--- sound-of-text.py
+++ sound-of-text.py
@@ -5,21 +5,29 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
+def click_on_download():
+    time.sleep(2)
+    download_btn = driver.find_element_by_link_text("DOWNLOAD")
+    download_btn.click()
+
 driver = webdriver.Chrome()
 driver.get("https://soundoftext.com/")
-driver.find_element_by_tag_name("textarea").send_keys("Hello")
 
 drop_down = Select(driver.find_element_by_tag_name("select"))
 drop_down.select_by_index(30)
 
-submit_btn = driver.find_element_by_tag_name("input")
-submit_btn.click()
+for i in range(1, 9999):
+    driver.find_element_by_tag_name("textarea").send_keys(str(i))
 
-time.sleep(2)
-#element = WebDriverWait(driver, 20).until( EC.element_to_be_clickable((By.LINK_TEXT, "DOWNLOAD")))
-#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "DOWNLOAD")))
+    submit_btn = driver.find_element_by_tag_name("input")
+    submit_btn.click()
 
-download_btn = driver.find_element_by_link_text("DOWNLOAD")
-#print(download_btn.get_attribute("href"))
-download_btn.click()
-##print(download_btn)
+    loaded = False
+
+    while not loaded:
+        try:
+            click_on_download()
+            loaded = True
+        except Exception as e:
+            print("Exception - ", str(i), "mp3 has not loaded yet")
+            loaded = False
